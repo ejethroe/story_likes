afbf92059c44cd2f9dd7bd8a1a7f213910202615
Evin <Evin@evin-jethroes-macbook.local>
2016-10-23 12:26:01 -0400

    added movie rating

--- story.rb
+++ story.rb
@@ -5,13 +5,19 @@
 
 
 #show the user a message that says "Welcome to Movie Rating"
+puts("Welccome to Movie rating")
 
 #show the user a message asking them for the title of a movie
+puts ("what's the title of a moveie")
 
 #store the input in a variable
+user_title = gets.strip
 
 #show the user a message asking for their rating of the movie out of ten
-
+puts ("what's the rating of the movie out of ten")
 # store the input in a variable
+user_rating = gets.strip
 
-display_story(user_title, user_rating)
+puts("#{user_title}, #{user_rating}") 
+
+#display_story(user_title, user_rating)
